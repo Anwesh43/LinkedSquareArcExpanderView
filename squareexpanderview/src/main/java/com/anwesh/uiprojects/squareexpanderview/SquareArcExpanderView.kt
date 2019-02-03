15d4cddd8137b0f423589f95faf20641db0b309b
Anwesh43 <anweshthecool0@gmail.com>
2019-02-04 03:58:06 +0530

    adding SquareArcExpander class

--- squareexpanderview/src/main/java/com/anwesh/uiprojects/squareexpanderview/SquareArcExpanderView.kt
+++ squareexpanderview/src/main/java/com/anwesh/uiprojects/squareexpanderview/SquareArcExpanderView.kt
@@ -179,4 +179,28 @@ class SquareArcExpanderView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class SquareArcExpander(var i : Int) {
+
+        private val root : SAENode = SAENode(0)
+        private var curr : SAENode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }

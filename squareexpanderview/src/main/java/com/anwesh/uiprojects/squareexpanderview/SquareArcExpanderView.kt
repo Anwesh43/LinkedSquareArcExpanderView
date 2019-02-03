552ff3b02da98d77c1b69e47d7a7ef22904c19dd
Anwesh43 <anweshthecool0@gmail.com>
2019-02-04 03:54:10 +0530

    adding SAENode class

--- squareexpanderview/src/main/java/com/anwesh/uiprojects/squareexpanderview/SquareArcExpanderView.kt
+++ squareexpanderview/src/main/java/com/anwesh/uiprojects/squareexpanderview/SquareArcExpanderView.kt
@@ -134,4 +134,49 @@ class SquareArcExpanderView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class SAENode(var i : Int, val state : State = State()) {
+
+        private var next : SAENode? = null
+        private var prev : SAENode? = null
+        private var dir : Int = 1
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = SAENode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSAENode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SAENode {
+            var curr : SAENode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }

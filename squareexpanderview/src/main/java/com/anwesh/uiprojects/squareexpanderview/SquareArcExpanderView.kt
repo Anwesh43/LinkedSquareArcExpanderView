3f4384241ceee0e81a49961d73657000d170917c
Anwesh43 <anweshthecool0@gmail.com>
2019-02-04 04:21:25 +0530

    adding fixes

--- squareexpanderview/src/main/java/com/anwesh/uiprojects/squareexpanderview/SquareArcExpanderView.kt
+++ squareexpanderview/src/main/java/com/anwesh/uiprojects/squareexpanderview/SquareArcExpanderView.kt
@@ -18,7 +18,7 @@ val lines : Int = 4
 val scGap : Float = 0.05f
 val scDiv : Double = 0.51
 val strokeFactor : Int = 90
-val sizeFactor : Float = 2.8f
+val sizeFactor : Float = 2.2f
 val foreColor : Int = Color.parseColor("#0D47A1")
 val backColor : Int = Color.parseColor("#BDBDBD")
 val rotDeg : Float = 90f
@@ -30,13 +30,13 @@ fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
 fun Float.mirrorValue(a : Int, b : Int) : Float = (1 - scaleFactor()) * a.inverse() + scaleFactor() * b.inverse()
 fun Float.updateValue(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap
 
-fun Canvas.drawExpanderPath(r : Float, paint : Paint) {
+fun Canvas.drawExpanderPath(r : Float, scale : Float, paint : Paint) {
     val sd : Int = 180
     val ed : Int = 360
     val path : Path = Path()
     for (j in sd..ed) {
-        val x : Float = Math.cos(j * Math.PI / 180).toFloat()
-        val y : Float = Math.sin(j * Math.PI / 180).toFloat()
+        val x : Float = r * Math.cos(j * Math.PI / 180).toFloat()
+        val y : Float = r * scale * Math.sin(j * Math.PI / 180).toFloat()
         if (j == sd) {
             path.moveTo(x, y)
         } else {
@@ -49,7 +49,7 @@ fun Canvas.drawExpanderPath(r : Float, paint : Paint) {
 fun Canvas.drawSAENode(i : Int, scale : Float, paint : Paint) {
     val w : Float = width.toFloat()
     val h : Float = height.toFloat()
-    val gap : Float = w / (nodes + 1)
+    val gap : Float = h / (nodes + 1)
     val size : Float = gap / sizeFactor
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     paint.strokeCap = Paint.Cap.ROUND
@@ -58,13 +58,13 @@ fun Canvas.drawSAENode(i : Int, scale : Float, paint : Paint) {
     val sc1 : Float = scale.divideScale(0, 2)
     val sc2 : Float = scale.divideScale(1, 2)
     save()
-    translate(gap * (i + 1), h / 2)
+    translate(w / 2, gap * (i + 1))
     rotate(rotDeg * sc2)
     for (j in 0..(lines - 1)) {
         save()
         rotate(rotDeg * j)
-        translate(0f, -size)
-        drawExpanderPath(size * sc1.divideScale(j, lines), paint)
+        translate(0f, -size / 2)
+        drawExpanderPath(size / 2, sc1.divideScale(j, lines), paint)
         restore()
     }
     restore()

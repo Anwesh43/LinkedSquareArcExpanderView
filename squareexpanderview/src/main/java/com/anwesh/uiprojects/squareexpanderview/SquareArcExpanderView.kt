b93c079c03b65866d43783c19abd4d6a581b02a5
Anwesh43 <anweshthecool0@gmail.com>
2019-02-04 03:37:20 +0530

    adding canvas extension methods

--- squareexpanderview/src/main/java/com/anwesh/uiprojects/squareexpanderview/SquareArcExpanderView.kt
+++ squareexpanderview/src/main/java/com/anwesh/uiprojects/squareexpanderview/SquareArcExpanderView.kt
@@ -21,6 +21,7 @@ val strokeFactor : Int = 90
 val sizeFactor : Float = 2.8f
 val foreColor : Int = Color.parseColor("#0D47A1")
 val backColor : Int = Color.parseColor("#BDBDBD")
+val rotDeg : Float = 90f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
@@ -29,3 +30,42 @@ fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
 fun Float.mirrorValue(a : Int, b : Int) : Float = (1 - scaleFactor()) * a.inverse() + scaleFactor() * b.inverse()
 fun Float.updateValue(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap
 
+fun Canvas.drawExpanderPath(r : Float, paint : Paint) {
+    val sd : Int = 180
+    val ed : Int = 360
+    val path : Path = Path()
+    for (j in sd..ed) {
+        val x : Float = Math.cos(j * Math.PI / 180).toFloat()
+        val y : Float = Math.sin(j * Math.PI / 180).toFloat()
+        if (j == sd) {
+            path.moveTo(x, y)
+        } else {
+            path.lineTo(x, y)
+        }
+    }
+    drawPath(path, paint)
+}
+
+fun Canvas.drawSAENode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = w / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.color = foreColor
+    paint.style = Paint.Style.STROKE
+    val sc1 : Float = scale.divideScale(0, 2)
+    val sc2 : Float = scale.divideScale(1, 2)
+    save()
+    translate(gap * (i + 1), h / 2)
+    rotate(rotDeg * sc2)
+    for (j in 0..(lines - 1)) {
+        save()
+        rotate(rotDeg * j)
+        translate(0f, -size)
+        drawExpanderPath(size * sc1.divideScale(j, lines), paint)
+        restore()
+    }
+    restore()
+}

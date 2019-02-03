6ef3227763f90aaf684e6f59243284519e980271
Anwesh43 <anweshthecool0@gmail.com>
2019-02-04 03:21:57 +0530

    importing libraries and defining constants

--- squareexpanderview/src/main/java/com/anwesh/uiprojects/squareexpanderview/SquareArcExpanderView.kt
+++ squareexpanderview/src/main/java/com/anwesh/uiprojects/squareexpanderview/SquareArcExpanderView.kt
@@ -3,3 +3,21 @@ package com.anwesh.uiprojects.squareexpanderview
 /**
  * Created by anweshmishra on 04/02/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.app.Activity
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Path
+import android.graphics.Color
+
+val nodes : Int = 5
+val lines : Int = 4
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.8f
+val foreColor : Int = Color.parseColor("#0D47A1")
+val backColor : Int = Color.parseColor("#BDBDBD")
